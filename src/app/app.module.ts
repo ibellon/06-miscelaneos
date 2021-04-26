463476c9e20f679e7d3b28edce69767293509338
ibellon <isidro.bellon@gmail.com>
2021-04-26 10:59:23 +0200

    css para un solo componente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgStyleComponent } from './components/ng-style/ng-style.component';
+import { CssComponent } from './components/css/css.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgStyleComponent
+    NgStyleComponent,
+    CssComponent
   ],
   imports: [
     BrowserModule,
